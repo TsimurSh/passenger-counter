9e39e4d669c3a09aa7f8b20f136bd7733867762f
Timur S. <nut.t@icloud.com>
2025-08-12 17:30:05 +0200

    pi-sniffer impl: WIP

--- src/main/java/pl/goeuropa/counter/repository/PeopleCountRepository.java
+++ src/main/java/pl/goeuropa/counter/repository/PeopleCountRepository.java
@@ -4,24 +4,19 @@ import lombok.Data;
 import pl.goeuropa.counter.configs.CapacitiesConfig;
 import pl.goeuropa.counter.dto.BusLoadDto;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Data
 public class PeopleCountRepository {
 
-    public static final Map<String, Integer> CAPACITY_CONFIGS = new CapacitiesConfig()
-            .getVehiclesAndDivisors();
+// TODO will uncomment soon
+//    public static final Map<String, Integer> CAPACITY_CONFIGS = new CapacitiesConfig()
+//            .getVehiclesAndDivisors();
 
     private static final PeopleCountRepository singleton = new PeopleCountRepository();
 
     private final Map<String, BusLoadDto> updatesAboutLoads = new ConcurrentHashMap<>();
-
-    private String jSessionId = null;
-    private String vehiIdnosParam = null;
-    private Set<String> vehicleIds = new HashSet<>();
 
     private PeopleCountRepository() {
     }
