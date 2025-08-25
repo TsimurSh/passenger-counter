26a139c837689e9d8b2b9a8b14891bba8d57ef83
Timur S. <nut.t@icloud.com>
2025-08-25 14:10:27 +0200

    fix: regex in dto

--- src/main/java/pl/goeuropa/counter/dto/BusLoadDto.java
+++ src/main/java/pl/goeuropa/counter/dto/BusLoadDto.java
@@ -17,18 +17,21 @@ public class BusLoadDto implements Serializable {
     private long timestamp;
 
     public BusLoadDto(LogEntryDto logEntry) {
-        this.vehicleName = parseMessage(logEntry.getMessage(),1);
-        this.currentCount = Integer.parseInt(parseMessage(logEntry.getMessage(),2));
+        parseMessage(logEntry.getMessage());
         this.currentFullness = Float.NaN;
         this.timestamp = logEntry.getTimestamp() / 1000;
     }
 
-    private String parseMessage(String message, int group) {
-        Pattern pattern = Pattern.compile("^\\s*(\\w+)\\s+([0-9]+(?:\\.[0-9]+)?)");
+    private void parseMessage(String message) {
+        Pattern pattern = Pattern.compile("^\\s*(\\S+)\\s+([0-9]+(?:\\.[0-9]+)?)");
         Matcher matcher = pattern.matcher(message);
 
-        if (matcher.find() && group == 2) return matcher.group(group);
-        if (matcher.find() && group == 2) return matcher.group(group);
-        return "";
+        if (matcher.find()) {
+            this.vehicleName = matcher.group(1);
+            this.currentCount = (int) Float.parseFloat(matcher.group(2));
+        } else {
+            this.vehicleName = "Unknown";
+            this.currentCount = -1;
+        }
     }
 }
